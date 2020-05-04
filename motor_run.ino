5cda52277ef06e1be5fe70db10df543bf5f8d71e
nlhs-Sam-Wu-1117 <h4572306@gmail.com>
2020-05-04 16:07:52 +0800

    合併藍芽及馬達控制

---
+++ motor_run.ino
@@ -0,0 +1,53 @@
+#include <AFMotor.h>
+#include <SoftwareSerial.h>
+AF_DCMotor motorL(3);
+AF_DCMotor motorR(4);
+const int rxpin = A0; // 接收 pin
+const int txpin = A1; // 發送 pin
+SoftwareSerial bluetooth(rxpin, txpin);
+
+void setup() {
+  motorL.setSpeed(200);
+  motorL.run(RELEASE);
+  motorR.setSpeed(200);
+  motorR.run(RELEASE);
+
+  Serial.begin(9600);
+  Serial.setTimeout(100);
+  Serial.println("Serial ready");
+  bluetooth.begin(9600);
+  bluetooth.setTimeout(100);
+  Serial.println("Bluetooth ready");
+  
+}
+  
+void loop() {
+  int command = GetCommand();
+  ExecCommand(command);
+
+}
+int GetCommand() {
+  int command = bluetooth.parseInt();
+  
+  return command;
+}
+
+void ExecCommand(int command){
+
+  if(command==1) {
+    motorL.run(RELEASE);
+    motorR.run(RELEASE);
+    Serial.println("stop");
+  }
+  else if(command==2) {
+    motorL.run(FORWARD);
+    motorR.run(FORWARD);
+    Serial.println("forward");
+  }
+  else if(command==3) {
+    motorL.run(BACKWARD);
+    motorR.run(BACKWARD);
+    Serial.println("backward");
+  }
+
+}
